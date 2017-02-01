a867c2727ee07179b997cb3cb3f351c28101a322
Jose Juarez <jose@juarez.me>
2017-01-31 22:48:30 -0500

    moved showing stories into a function

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 import json, requests
 
+
+### Variable Declaration ###
 url = 'http://mashable.com/stories.json?new_per_page=60'
 data = requests.get(url)
 doc = json.loads(data.content)
 
 # get channeles
 categories = []
-
 # go through all stories and add their channel to the categories list
 for story in doc['new']:
     categories.append(story['channel'])
-
 # sort out uniques in list using a set and back to list
 categories = sorted(list(set(categories)))
 
-for story in doc['new']:
-    print("""
-        ++++++++++++++++++
-        Title: {}
-        ---
-        Excerpt: {}
-        ---
-        URL: {}
+def show_stories():
+    for story in doc['new']:
+        print("""
+            ++++++++++++++++++
+            Title: {}
+            ---
+            Excerpt: {}
+            ---
+            URL: {}
 
-        ++++++++++++++++++""".format(story['title'], story['excerpt'], story['link']))
+            ++++++++++++++++++""".format(story['title'], story['excerpt'], story['link']))
+
+# show_stories()
